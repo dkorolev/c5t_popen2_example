1bcc07bb0a21ad6a9731aba59038dbd386f5b234
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-04-18 00:52:29 +0200

    cleaner!

--- src/lib_c5t_lifetime_manager.h
+++ src/lib_c5t_lifetime_manager.h
@@ -359,17 +359,19 @@ T& CreateLifetimeTrackedInstance(char const* file, int line, std::string const& 
 //                 to eliminate the risk of this thread being `.join()`-ed before it is fully done.
 // NOTE(dkorolev): The `ready_to_go` part is essential because otherwise the lambda capture list may not intiailize yet!
 // TODO(dkorolev): Why and how so though? I better investigate this deeper before using `std::move`-d lambda captures!
-#define LIFETIME_TRACKED_THREAD(desc, body)                        \
-  do {                                                             \
-    current::WaitableAtomic<bool> ready_to_go(false);              \
-    LIFETIME_MANAGER_SINGLETON_IMPL().EmplaceThreadImpl([&]() {    \
-      auto& mgr = LIFETIME_MANAGER_SINGLETON_IMPL();               \
-      size_t const id = mgr.TrackingAdd(desc, __FILE__, __LINE__); \
-      ready_to_go.SetValue(true);                                  \
-      body();                                                      \
-      mgr.TrackingRemove(id);                                      \
-    });                                                            \
-    ready_to_go.Wait([](bool b) { return b; });                    \
+#define LIFETIME_TRACKED_THREAD(desc0, body)                                                   \
+  do {                                                                                         \
+    current::WaitableAtomic<bool> ready_to_go(false);                                          \
+    std::string desc(desc0);                                                                   \
+    LIFETIME_MANAGER_SINGLETON_IMPL().EmplaceThreadImpl(                                       \
+        [moved_desc = std::move(desc), moved_body = std::move(body), &ready_to_go]() mutable { \
+          auto& mgr = LIFETIME_MANAGER_SINGLETON_IMPL();                                       \
+          size_t const id = mgr.TrackingAdd(moved_desc, __FILE__, __LINE__);                   \
+          ready_to_go.SetValue(true);                                                          \
+          moved_body();                                                                        \
+          mgr.TrackingRemove(id);                                                              \
+        });                                                                                    \
+    ready_to_go.Wait([](bool b) { return b; });                                                \
   } while (false)
 
 // TODO(dkorolev): This `#ifdef` is ugly, and it will get fixed once we standardize our `cmake`-based builds.
